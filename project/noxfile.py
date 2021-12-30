e0a7e21a89b2cf77484029fce717e3afa3f3edfc
Vincent Leroy <vincent.leroy@rayference.eu>
2021-12-30 18:55:26 +0100

    Nox setup: Use duties

--- project/noxfile.py
+++ project/noxfile.py
@@ -7,5 +7,5 @@ os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
 @nox.session(python=("3.7", "3.8", "3.9", "3.10"))
 def test(session):
-    session.run("pdm", "install", "-G", "tests", external=True)
-    session.run("pytest", "tests/")
+    session.run("pdm", "install", "-G", "tests", "-G", "duty", external=True)
+    session.run("duty", "test/")
