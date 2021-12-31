a927f0831b89c163f3b21642833cebbf246746a5
Vincent Leroy <vincent.leroy@rayference.eu>
2021-12-31 09:14:31 +0100

    Simplify noxfile

--- project/noxfile.py
+++ project/noxfile.py
@@ -1,32 +1,23 @@
+import os
+
 import nox
 
 
-def _no_venv(session):
-    return isinstance(session.virtualenv, nox.virtualenv.PassthroughEnv)
-
-
 def _has_venv(session):
-    return not _no_venv(session)
-
-
-def _pdm(session):
-    if _no_venv(session):
-        return ["pdm"]
-    else:
-        return ["pdm", "--ignore-python"]
+    return not isinstance(session.virtualenv, nox.virtualenv.PassthroughEnv)
 
 
 @nox.session(python=("3.7", "3.8", "3.9", "3.10"))
 def test(session):
     """Run the test suite."""
-    pdm = _pdm(session)
 
-    # Install only if a virtual environment is used
+    # If a virtual environment is used, configure PDM appropriately and install
     # If --no-venv is used, the install step is skipped
     if _has_venv(session):
-        session.run(*pdm, "install", "-G", "tests", external=True)
+        os.environ.update({"PDM_USE_VENV": "1", "PDM_IGNORE_SAVED_PYTHON": "1"})
+        session.run("pdm", "install", "-G", "tests", external=True)
 
     # Allow passing arguments to pytest
     # Example: nox --no-venv -s tests -- tests/my_test.py
     args = session.posargs if session.posargs else ["tests"]
-    session.run(*pdm, "run", "pytest", "-c", "config/pytest.ini", *args, external=True)
+    session.run("pdm", "run", "pytest", "-c", "config/pytest.ini", *args, external=True)
